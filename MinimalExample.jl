8ad33763813f2b4ad5a751f52309354d2077a28e
Jan Lisse <jan.lisse@posteo.de>
2019-08-27 14:52:18 +0200

    cleanup code

--- MinimalExample.jl
+++ MinimalExample.jl
@@ -5,18 +5,20 @@ using OrdinaryDiffEq: ODEProblem, Rodas4, init, solve!,solve, step!, reinit!,sav
 struct RootRhs
     rhs
 end
+
 function (rr::RootRhs)(x)
     dx = similar(x)
-    rr.rhs(dx, x, 1.0, 0.)
+    rr.rhs(dx, x, nothing, 0.)
     dx
 end
 
-
 struct NetworkEq
-    P_1
+    P
 end
 
-function (neq::NetworkEq)(dx, x, p ,t)
+NetworkEq(; P) = NetworkEq(P)
+
+function (neq::NetworkEq)(dx, x, params ,t)
     admittance = -1im/0.02
     Y_matrix = [-admittance admittance;
                 admittance -admittance]
@@ -39,7 +41,11 @@ function (neq::NetworkEq)(dx, x, p ,t)
     i_2 = i[2]
     p = real(u .* conj(i))
     dϕ_1 = ω_1
-    dω_1 = ((neq.P_1 - D_1 * ω_1) - p[1]) * Ω_H_1
+    if (params != nothing)
+        dω_1 = (((neq.P*params) - D_1 * ω_1) - p[1]) * Ω_H_1
+    else
+        dω_1 = ((neq.P - D_1 * ω_1) - p[1]) * Ω_H_1
+    end
     v_1 = abs(u_1)
     dv_1 = -Γ * (v_1 - V)
     du_1 = (u_1 / v_1) * dv_1 + u_1 * im * dϕ_1
@@ -127,10 +133,10 @@ function simulate_switch_rhs(rhs1, rhs2, x0, timespan,tspan_fault)
 end
 
 
-function find_operationpoint1(rr::RootRhs, ic_guess = nothing)
-    system_size = 6
-    ic_guess = ones(system_size)
-    nl_res = nlsolve(rr, ic_guess)
+function find_operationpoint(rhs)
+    ic_guess = ones(6) ##hardcoded, 2 nodes, each with 3 vars
+    root_rhs=RootRhs(rhs)
+    nl_res = nlsolve(root_rhs, ic_guess)
     if converged(nl_res) == true
         return nl_res.zero
     else
@@ -139,88 +145,49 @@ function find_operationpoint1(rr::RootRhs, ic_guess = nothing)
 end
 
 
-function rhs!(dx, x, params, t)
-    admittance = -1im/0.02
-    Y_matrix = [-admittance admittance;
-                admittance -admittance]
-    P_1 =1
-    H_1 =5
-    P_2 = -1
-    H_2 =5
-    D_1=0.1
-    D_2=1
-    Ω_H_1 = 2*π*50/H_1
-    Ω_H_2 = 2*π*50/H_2
-    Γ=0.1
-    V=1.
-    u_1 = x[1] + x[2] * im
-    ω_1 = x[3]
-    u_2 = x[4] + x[5] * im
-    ω_2 = x[6]
-    u = [u_1;u_2]
-    i = -Y_matrix*u
-    i_1 = i[1]
-    i_2 = i[2]
-    p = real(u .* conj(i))
-    dϕ_1 = ω_1
-    dω_1 = (((params*P_1) - D_1 * ω_1) - p[1]) * Ω_H_1
-    v_1 = abs(u_1)
-    dv_1 = -Γ * (v_1 - V)
-    du_1 = (u_1 / v_1) * dv_1 + u_1 * im * dϕ_1
-
-    dϕ_2 = ω_2
-    dω_2 = ((P_2 - D_2 * ω_2) - p[2]) * Ω_H_2
-    v_2 = abs(u_2)
-    dv_2 = -Γ * (v_2 - V)
-    du_2 = (u_2 / v_2) * dv_2 + u_2 * im * dϕ_2
-
-    dx[1] = real(du_1)
-    dx[2] = imag(du_1)
-    dx[3] = dω_1
-    dx[4] = real(du_2)
-    dx[5] = imag(du_2)
-    dx[6] = dω_2
-    return nothing
-end
-
 function run_sim_params()
-    root_rhs=RootRhs(rhs!)
-    operationpoint = find_operationpoint1(root_rhs)
+    rhs_stable = NetworkEq(1)
+    operationpoint = find_operationpoint(rhs_stable)
+    tspan_sim =   (0.,1.)
     tspan_fault = (0.1,1)
     disturbed_node=2
-    sol1 = simulate_with_param(rhs!,
+    sol1 = simulate_with_param(rhs_stable,
         operationpoint,
-        (0., 1.),
+        tspan_sim,
         tspan_fault)
     plot(sol1, vars=[3,6])
 end
 
 function run_sim_hack()
-    root_rhs=RootRhs(rhs!)
-    operationpoint = find_operationpoint1(root_rhs)
+
+    rhs_stable = NetworkEq(1)
+    rhs_fault = NetworkEq(0.9)
+    operationpoint = find_operationpoint(rhs_stable)
+    tspan_sim =   (0.,1.)
     tspan_fault = (0.1,1)
     disturbed_node=2
-    netw = NetworkEq(1)
-    netw_drop = NetworkEq(0.9)
-    sol = simulate_hack(netw,netw_drop,
+
+    sol = simulate_hack(rhs_stable,rhs_fault,
         operationpoint,
-        (0., 1.),
+        tspan_sim,
         tspan_fault)
     plot(sol, vars=[3,6])
 end
 
 function run_sim_switch_rhs()
-    root_rhs=RootRhs(rhs!)
-    operationpoint = find_operationpoint1(root_rhs)
+
+    rhs_stable = NetworkEq(1)
+    rhs_fault = NetworkEq(0.9)
+    operationpoint = find_operationpoint(rhs_stable)
+    tspan_sim =   (0.,1.)
     tspan_fault = (0.1,1)
     disturbed_node=2
-    netw = NetworkEq(1)
-    netw_drop = NetworkEq(0.9)
-    sol4 = simulate_switch_rhs(netw,netw_drop,
+
+    sol = simulate_switch_rhs(rhs_stable,rhs_fault,
         operationpoint,
-        (0., 1.),
+        tspan_sim,
         tspan_fault)
-    plot(sol4, vars=[3,6])
+    plot(sol, vars=[3,6])
 end
 
 #-
